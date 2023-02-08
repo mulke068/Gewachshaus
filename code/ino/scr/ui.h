5b424201d30a0c30b75fe754ea2ca9e2f41b24ca
mulke068 <69611757+mulke068@users.noreply.github.com>
2023-02-08 12:22:43 +0100

    Update ui.h

--- code/ino/scr/ui.h
+++ code/ino/scr/ui.h
@@ -2,10 +2,11 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
 
-
-int TextPrint(int size=0,char color="",int cursor_x=0,int cursor_y=0,char text=""){
-  tft.setTextSize(size);
-  tft.setTextColor(color);
-  tft.setCursor(cursor_x,cursor_y);
-  tft.print(text);
-}
+/*
+  int TextPrint(int size=0,char color="",int cursor_x=0,int cursor_y=0,char text=""){
+    tft.setTextSize(size);
+    tft.setTextColor(color);
+    tft.setCursor(cursor_x,cursor_y);
+    tft.print(text);
+  }
+*/
