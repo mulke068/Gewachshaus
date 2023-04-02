b4d726352309e240beb94d40404658ce9694bbf4
mulke068 <69611757+mulke068@users.noreply.github.com>
2023-04-02 22:31:10 +0200

    Revert "Merge remote-tracking branch 'origin/12-website' into 12-website"
    This reverts commit 3d15af61aa72bef007ed296eaa16cdbf2323fe9d.

---
+++ code/www/nuxt/nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      api_host: (process.env.NUXT_PUBLIC_API_HOST || "http://localhost:8080"),
+    }
+  },
+  // ...
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  nitro: {},
+  /*
+  server: {
+    port: 8000, // default: 3000
+    host: '0.0.0.0', // default: localhost,
+    timing: false
+  },
+  */
+  components: true,
+});
