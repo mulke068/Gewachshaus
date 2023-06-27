f400a863faa2b4ac04d65f51907de27a59866511
mulke068 <69611757+mulke068@users.noreply.github.com>
2023-06-28 01:56:31 +0200

    Delete code/www/nuxt_bkp directory

--- code/www/nuxt_bkp/server/api/sensor/id/[id].get.ts
+++
@@ -1,6 +0,0 @@
-export default defineEventHandler(async (event) => {
-    const config = useRuntimeConfig();
-    const id = event.context.params.id;
-    const data = await $fetch(`${config.public.api_host}/sensor/id/${id}`);
-    return data;
-})
--- code/www/nuxt_bkp/server/api/settings/index.get.ts
+++
@@ -1,5 +0,0 @@
-export default defineEventHandler(async (event) => {
-    const config = useRuntimeConfig();
-    const data = await $fetch(`${config.public.api_host}/settings`);
-    return data;
-})
