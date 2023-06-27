216d055d2b8fd5f3c9eb24b63e5d136ae51d004c
mulke068 <mullerkevin2005@hotmail.com>
2023-06-27 14:39:37 +0200

    Update [slug].vue

--- code/www/nuxt/pages/blog/[slug].vue
+++ code/www/nuxt/pages/blog/[slug].vue
@@ -1,10 +1,20 @@
 import { ContentDoc } from '../../.nuxt/components';
-<script lang="ts" setup>
-
-</script>
-
 <template>
     <main>
         <ContentDoc />
     </main>
 </template>
+
+
+<style scoped>
+main {
+    @apply p-4 
+}
+main :deep(h1) {
+    @apply text-2xl p-4
+}
+main :deep(h2) {
+    @apply text-lg p-2
+}
+
+</style>
